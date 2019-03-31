0aaf9740bbd3ce4e9581a0a298067dfb9a5edb44
ywardaoui <w.youness@msn.com>
2019-03-30 23:45:43 -0400

    registration form added

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class user {
+    username: String;
+    password: String; 
+    avatar: String;
+    firstName: String;
+    lastName: String;
+    email: String;
+    birthday: String;
+    gender: String;
+}
