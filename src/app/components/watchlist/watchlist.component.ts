aa2fa7b7ed2dfcee151892e56e3d623169309213
ywardaoui <w.youness@msn.com>
2019-04-04 23:07:18 -0400

    fixed favorit-added wish list

--- src/app/components/watchlist/watchlist.component.ts
+++ src/app/components/watchlist/watchlist.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { MovieService } from "src/app/services/movie.service";
+import { MoviesDetail } from 'src/app/models/movieDetail';
+import { wishMovy } from 'src/app/models/favoriteMovieModel';
+import { Router } from '@angular/router';
+
+interface wishMovieInfo
+{
+  id: number;
+  imgPath: string;
+  title: string;
+}
 
 @Component({
   selector: "app-watchlist",
@@ -7,21 +17,50 @@ import { MovieService } from "src/app/services/movie.service";
   styleUrls: ["./watchlist.component.css"]
 })
 export class WatchlistComponent implements OnInit {
-  constructor(private movieService: MovieService) {}
-
+  constructor(private movieService: MovieService, private route: Router) {}
   ngOnInit() {
-    this.getWatchList();
+    this.getWishMovie()
   }
 
-  wishMovies: object[];
+  imgUrl: string = "http://image.tmdb.org/t/p/w300";
+  wishMoviesIds: wishMovy[];
+  wishMovies: wishMovieInfo[] = [];
+  movieDeet: MoviesDetail;
+  
+  buildMovieArray()
+  {
+    console.log(this.wishMoviesIds);
 
-  getWatchList() {
+    for(let i = 0; i < this.wishMoviesIds.length; ++i)
+    {
+      localStorage.setItem("movieId", ""+this.wishMoviesIds[i].id);
+
+      this.movieService.getMovieDetail().then(res => {
+        let tempy:wishMovieInfo ={
+          id: res.id,
+          imgPath: res.poster_path,
+          title: res.title
+        }; 
+        
+        this.wishMovies.push(tempy);
+        
+      }).catch(e => console.log(e));
+     
+    }
+  }
+
+  redirectUrl(id: string) {
+    localStorage.setItem("movieId", id);
+    this.route.navigateByUrl("movies/movie-detail");
+  }
+
+  getWishMovie() {
     this.movieService
-      .getWatchList()
+      .getWishMovie()
       .then(res => {
-        console.log(res);
-        this.wishMovies = res.wishList;
+        this.wishMoviesIds = res.wishList;
       })
-      .catch(e => console.log(e));
+      .catch(e => console.log(e))
+      .finally(() =>  this.buildMovieArray());
   }
 }
